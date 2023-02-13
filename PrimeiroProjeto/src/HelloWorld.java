e53285d7efff3d797f4b0fb164f5d16c47ab741a
Cynthia <cynthiaareasjardim@gmail.com>
2023-02-13 19:59:00 -0300

    Primeiro commit. Adiciona pasta de proeto de estudo em java

---
+++ PrimeiroProjeto/src/HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld {
+
+	/*métodos estáticos não dependem do objeto para serem chamados*/
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Testando o comando de output!");//o ln pula de linha
+		
+	}
+	
+}
